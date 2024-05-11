e2f567b2b71d407e9cf90b681ea0881d364f2796
Srikanth2810 <lakshmi2003sri@gmail.com>
2024-05-11 21:46:27 +0530

    Second commit

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://srikanthr-consultancy.netlify.app/login'
+    // base: 'https://srikanthr-consultancy.netlify.app/login'
 })
